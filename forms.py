57c1bf21d659322f1e4f3a39608f52bd7b758220
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-10-01 07:57:03 +0300

    start using bootstrap and creating forms

---
+++ forms.py
@@ -0,0 +1,12 @@
+# type: ignore
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField 
+from wtforms.validators import DataRequired, length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Username', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
--- iticket.py
+++ iticket.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+# type: ignore
+from flask import Flask, render_template, url_for, flash
+
+# below is another way to integrate "Bootstrap" into out app
+# from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+# below is another way to integrate "Bootstrap" into out app
+# bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 @app.route("/home")
 def home():
-    return "hello I am ITicket"
+    return render_template("testbootstrap.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title="ITicket - Page not found"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html', title="Internal server error"), 500
 
 
 if __name__ == '__main__':
