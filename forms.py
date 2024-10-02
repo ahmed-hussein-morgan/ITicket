4e7560766cef1efb1c1f304937d6273d9d8eca44
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-10-02 08:14:50 +0300

    configure the secretkey and start creating the login form

--- forms.py
+++ forms.py
@@ -4,9 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, Email
 from wtforms.validators import DataRequired, length, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Username', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
 
--- iticket.py
+++ iticket.py
@@ -1,11 +1,13 @@
 # type: ignore
 from flask import Flask, render_template, url_for, flash
+from forms import LoginForm
+import os
 
 # below is another way to integrate "Bootstrap" into out app
 # from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # below is another way to integrate "Bootstrap" into out app
 # bootstrap = Bootstrap(app)
 
@@ -14,6 +16,13 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("testbootstrap.html")
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="login", form=form)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
