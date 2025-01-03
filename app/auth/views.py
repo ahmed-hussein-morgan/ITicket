48b65ef6411cbb567866547959aee747840514f1
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2025-01-03 15:48:32 +0200

    fix routing protecting new user by login page

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,11 +11,12 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(id=form.user_id.data).first()
-        if user and user.check_password(password_hash, form.password.data):
+        if user and user.check_password(form.password.data):
+        # if user and user.check_password(password_hash, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            current_user_type = User.query.filter_by(role_type).first()
-            current_user_name = User.query.filter_by(employee_name).first()
+            current_user_type = User.query.filter_by(role_type=user.role_type).first()
+            current_user_name = User.query.filter_by(employee_name=user.employee_name).first()
 
             if current_user_type == "tech":
                 flash(f"Welcome {current_user_name}.", 'success')
@@ -31,7 +32,7 @@ def login():
         else:
             flash(f"Login Failed. Please check User ID and password", "dangerous")
 
-    return render_template('auth/login.html')
+    return render_template('login.html', form=form)
 
 
 @auth.route('/logout')
